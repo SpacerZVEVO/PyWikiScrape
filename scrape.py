1715e3505c8eda6465d9d7be992420e5dc96eaef
SpacerZ <49669886+SpacerZVEVO@users.noreply.github.com>
2022-05-17 14:07:35 -0400

    Fixed various parsing errors

--- scrape.py
+++ scrape.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python
 import requests
 import random
+import re
 from bs4 import BeautifulSoup
 """Script searches for random Wikipedia articles starting from a base link to the set depth."""
 
 __author__ = "SpacerZ"
 __date__ = "7/23/2021"
 
-STARTING_POINT_URL = "https://en.wikipedia.org/wiki/United_States_Naval_Research_Laboratory"
-# ! Change this to search deeper into Wikipedia from the starter URL.
-DEPTH = 1
+WIKI_REGEX = re.compile(r'.*\/wiki\/[^:%]*')
+# ! Config values
+STARTING_POINT_URL = "https://en.wikipedia.org/wiki/Social_commerce"
+DEPTH = 20
 
 def scrape_wikipedia_webpages(url):
     webpage_data = requests.get(url)
-
     soup = BeautifulSoup(webpage_data.content, 'html.parser')
+    webpage_a_elements = soup.find(id='bodyContent').find_all('a')
+    potential_webpages = []
 
-    title = soup.find(id='firstHeading')
+    for a in webpage_a_elements:
+        if WIKI_REGEX.search(str(a)):
+            potential_webpages += a
 
-    potential_webpages = soup.find(id='bodyContent').find_all('a')
+    # ! Throws an error if no other links were scraped.
+    if len(potential_webpages) == 0:
+        raise Exception('No links were scraped!')
 
     random.shuffle(potential_webpages)
+    selected_link = str(potential_webpages[0])
+    selected_link = selected_link.replace(" ", "_")
 
-    selected_webpage = 0
-
-    for webpage in potential_webpages:
-        if webpage['href'].find('/wiki/') == -1:
-            continue
-
-        selected_webpage = webpage
-        break
-
-    return "https://en.wikipedia.org" + selected_webpage['href']
+    output = "https://en.wikipedia.org/wiki/" + selected_link
+    print('Output value : ' + output)
+    return output
 
 final_webpage_url = STARTING_POINT_URL
 for search_depth in range(DEPTH):
